f5227e0fad6876598b12cea06c082b7c15d6b633
“seeinplays” <seeinplays@gmail.com>
2022-06-26 00:09:12 -0600

    Pokt

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,16 @@
-
-
-const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
-    const accountBalance = await deployer.getBalance();
-
-   
+async function main() {
+    // We get the contract to deploy
+    const Notification = await ethers.getContractFactory("Notification");
+    const notification = await Notification.deploy();
   
-    console.log("Deploying contracts with account: ", deployer.address);
-    console.log("Account balance: ", accountBalance.toString());
+    await notification.deployed();
   
-    await waveContract.deployed();
+    console.log("Notification deployed to:", notification.address);
+  }
   
-    console.log("WavePortal address: ", waveContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
       process.exit(1);
-    }
-  };
-  
-  runMain();
+    });
