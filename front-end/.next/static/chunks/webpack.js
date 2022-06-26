979a6b4dd21c504785295b64634d0a063a1fcce9
Daiana Bilbao <dbilbao@cs.colostate.edu>
2022-06-25 20:21:44 -0400

    deleted files

--- front-end/.next/static/chunks/webpack.js
+++ front-end/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa01599e3533d6a1"; }
+/******/ 		__webpack_require__.h = function() { return "6dec1dc990f04f06"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -28,7 +28,7 @@ const Menu = () => {
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">{companyName}</span>
-                  <img alt="logo" className="h-16 w-auto sm:h-16" src={logo} />
+                  <img alt="logo" className="h-10 w-auto sm:h-10" src={logo} />
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button
