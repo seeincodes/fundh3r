af2467a72a6e062aec7fca2fcf464b8c27c1aaf3
seeinplays <100040464+seeinplays@users.noreply.github.com>
2022-06-26 00:27:13 -0600

    Epns (#9)
    * Tatum
    
    * Tatum
    
    Co-authored-by: “seeinplays” <“seeinplays@gmail.com”>

--- front-end/.next/static/chunks/webpack.js
+++ front-end/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0fa649176da3104"; }
+/******/ 		__webpack_require__.h = function() { return "1f462407ae1d3df0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ front-end/src/components/Tatum.tsx
@@ -0,0 +1,6 @@
+import { TatumEthSDK } from '@tatumio/eth'
+
+// pass API key from Tatum account available for free at https://dashboard.tatum.io/
+const ethSDK = TatumEthSDK({ apiKey: '<Your API Key>' })
+
+// pick Tatum service available from API docs
